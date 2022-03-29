903a4f4268190e0659305ffca8c1ff47ec56010f
Rana Beri <ranaberi25@gmail.com>
2022-03-29 13:29:44 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const changeData = document.querySelector(".changeData");
 const editBut = document.querySelector(".editBut");
 const userDat = document.querySelector(".userDat");
 
+const tabAdd = document.querySelector(".tabAdd");
+const tabFix = document.querySelector(".tabFix");
+const tabDel = document.querySelector(".tabDel");
+const tabself = document.querySelector(".tabself");
+
+const delBox  = document.querySelector(".delBox");
+const closeIt = document.querySelector(".closeIt");
+const delconf = document.querySelector(".delconf");
+
+const editor = document.querySelector(".editor");
+const closeEdit = document.querySelector(".closeEdit");
+
+const cursAdd = document.querySelector(".cursAdd");
+const closeAdd = document.querySelector(".closeAdd");
 
 
 addbtn.addEventListener("click" , ()=>{
@@ -23,3 +37,39 @@ editBut.addEventListener("click" , ()=>{
 changeData.addEventListener("click" , ()=>{
   userDat.style.display = "flex";
 })
+tabself.addEventListener("click" , ()=>{
+  tabAdd.style.display = "block";
+  tabFix.style.display = "block";
+  tabDel.style.display = "block";
+})
+
+tabDel.addEventListener("click" , ()=>{
+  tabAdd.style.display = "none";
+  tabFix.style.display = "none";
+  tabDel.style.display = "none";
+  delBox.style.display = "flex";
+})
+tabFix.addEventListener("click" , ()=>{
+  tabAdd.style.display = "none";
+  tabFix.style.display = "none";
+  tabDel.style.display = "none";
+  editor.style.display = "flex";
+})
+tabAdd.addEventListener("click" , ()=>{
+  tabAdd.style.display = "none";
+  tabFix.style.display = "none";
+  tabDel.style.display = "none";
+  cursAdd.style.display = "flex";
+})
+
+closeIt.addEventListener("click" , ()=>{
+  delBox.style.display = "none";
+})
+closeEdit.addEventListener("click" , ()=>{
+  editor.style.display = "none";
+})
+closeAdd.addEventListener("click" , ()=>{
+  cursAdd.style.display = "none";
+})
+
+
